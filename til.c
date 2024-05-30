8deec4ce801e4a807646d193529dd89c414b1a95
san-sae <97453758+san-sae@users.noreply.github.com>
2024-05-30 11:41:22 +0900

    Update til.c

--- til.c
+++ til.c
@@ -1,1 +1,2 @@
 #include<stdio.h>
+curl --include --request GET 'http://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getUltraSrtNcst?serviceKey='%2FBjlwrmTaQUJ%2BT7t0HV9jHG9moyokAtcZCywc3DMdNjByevERQcnWZPTWSKF1a57idoiEt3d1M2VtELI8OmSvQ%3D%3D'&pageNo=1&numOfRows=1000&dataType=XML&base_date=20240523&base_time=0600&nx=55&ny=127'
